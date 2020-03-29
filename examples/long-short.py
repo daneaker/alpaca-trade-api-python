9e446b1565c2aaab5a330a34a39177e337f036e9
daneaker <62772064+daneaker@users.noreply.github.com>
2020-03-28 20:50:09 -0400

    Update long-short.py

--- examples/long-short.py
+++ examples/long-short.py
@@ -3,8 +3,8 @@ import threading
 import time
 import datetime
 
-API_KEY = "YOUR_API_KEY_HERE"
-API_SECRET = "YOUR_API_SECRET_HERE"
+API_KEY = "PKV0K2PHQK7V87JX0259"
+API_SECRET = "palZFBtWEEvhYLrt0wOfSBK0uaRJTDtiLF12dC8i"
 APCA_API_BASE_URL = "https://paper-api.alpaca.markets"
 
 
